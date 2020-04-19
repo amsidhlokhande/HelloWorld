cfa89c0359391ab37bc096c939a7db038b0c16db
amsidhlokhande <amsidhlokhande@gmail.com>
2020-04-19 22:13:43 +0530

    Initial Commit

---
+++ src/hello/HelloWorld.java
@@ -0,0 +1,14 @@
+package hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+     
+		for(int i=1;i<=10;i++){
+			System.out.println("Hi Amsidh Hello World "+i);
+			
+		}
+
+	}
+
+}
